d4f1559fc42cb98dcb7b884c749e5b5ae473c80b
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:54:24 -0400

    Update game_functions.c

--- game_functions.c
+++ game_functions.c
@@ -11,11 +11,14 @@ char* deal(char* game_deck, int num_cards){
   */
 }
 
-char* setup_game(char game_ID, char* decks[4], int num_players){
+char* setup_game(char game_ID, char* decks[4], int num_players, char* discard_pile){
   /*
   This function will set up the game based on which game the setup script is given
   and how many people are playing. We have a hard-coded number of decks (4) because
   an empty deck is still a deck, even if it's not being used.
+
+  It's going to return the game deck in its current state after dealing to num_players
+  players.
   */
   char* deck;
   
@@ -24,9 +27,11 @@ char* setup_game(char game_ID, char* decks[4], int num_players){
       deck = 0x0; // CHANGE- Load UNO deck. 
       for(int i = 0; i < num_players; i++){
         // deal 7 random cards to each player
-        decks[i] = deal(deck = 7); // POSSIBLE BUG. CHECK NOTE BELOW.
-        // NOTE: does this pass by address to decks? It's supposed to
+        decks[i] = deal(deck, 7); // POSSIBLE BUG. CHECK NOTE BELOW.
+        // NOTE: does this pass by address to decks? It's supposed to.
       }
+      // add one card to the center
+      // initialize the discard pile to an empty deck-like structure
     
     break;
   
