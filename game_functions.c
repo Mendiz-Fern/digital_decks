ab0111812ff2f42c2256bc461f7f80a4501cf1d6
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:24:45 -0400

    Update game_functions.c
    Not even sure this change will stay for long

--- game_functions.c
+++ game_functions.c
@@ -1,2 +1,14 @@
 // "These are all the functions that are going to be performed by the computer/ the game without any direct input for the player.
 // one could say these are the functions that will be called most inside the player_fucntions." -Fern
+
+
+#include <stdio.h>
+#include "game_functions.h"
+
+int* get_deck(int player_ID){
+  /*
+  this will get the deck from player player_ID (number between 1 and 4).
+  Simple as that
+  */
+  return /*insert the list of decks*/[player_ID-1] 
+}
