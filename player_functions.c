95caced85e210b1cae4c0ebdc4a25f8b1c3880fc
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:12:33 -0400

    Update and rename game_functions.c to player_functions.c

--- player_functions.c
+++ player_functions.c
@@ -5,8 +5,10 @@
 #include <stdio.h>
 #include <game_functions.h>
 
-int* select_card(int* deck, int pos_in_deck){
-  /* Selects a card from a deck and returns a list of integers representing multiple things
+int* select_card(int player_ID, int pos_in_deck){
+  /* Selects a card from a deck and returns a list of integers representing multiple things. The deck
+     will be tied to the player #, so you can get a deck from a player by simply asking what deck a 
+     player had.
      
      The idea at the moment is that each card will have a specific card ID, and a deck can be
      represented as a list of these Card IDs, then to select a card, the code will simply see 
