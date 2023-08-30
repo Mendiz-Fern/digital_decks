38b0be4d078a6ec91f91975c40ec5a82e885dd19
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 14:50:46 -0400

    Added a comment lol

--- game_functions.c
+++ game_functions.c
@@ -1,2 +1,6 @@
+// "Here go the actual functions of the actions a player can perform. This will make it easier to implement
+// gameplay later on, by simply adding a GUI action to each function and triggers based on the controller
+// inputs." -Fern
+
 #include <stdio.h>
 
