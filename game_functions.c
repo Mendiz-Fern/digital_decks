a4407e8947479c74da26ddb18d5eccc6cd0c0299
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:16:31 -0400

    Update game_functions.c

--- game_functions.c
+++ game_functions.c
@@ -1,1 +1,2 @@
-
+// "These are all the functions that are going to be performed by the computer/ the game without any direct input for the player.
+// one could say these are the functions that will be called most inside the player_fucntions." -Fern
