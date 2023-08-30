9c1562500791345e4c9c7c0e8d354576b33164b4
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:02:31 -0400

    Added description for select_card

--- game_functions.c
+++ game_functions.c
@@ -5,4 +5,12 @@
 #include <stdio.h>
 #include <game_functions.h>
 
-
+int* select_card(int* deck, int pos_in_deck){
+  /* Selects a card from a deck and returns a list of integers representing multiple things
+     
+     The idea at the moment is that each card will have a specific card ID, and a deck can be
+     represented as a list of these Card IDs, then to select a card, the code will simply see 
+     what card is in position pos_in_deck of the deck, and return a list containing
+     [player_ID, card_ID]
+  */
+}
