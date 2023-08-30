531c58fe4928993008ef1a27510a1704fc466aba
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:42:12 -0400

    Added a setup_game function

--- game_functions.c
+++ game_functions.c
@@ -5,6 +5,32 @@
 #include <stdio.h>
 #include "game_functions.h"
 
+void setup_game(char game_ID, char* decks[4], int num_players){
+  /*
+  This function will set up the game based on which game the setup script is given
+  and how many people are playing. We have a hard-coded number of decks (4) because
+  an empty deck is still a deck, even if it's not being used.
+  */
+
+  switch(game_ID):
+    case 0x1: // Setup for UNO
+      // Load UNO deck. 
+      for(int i = 0; i < num_players; i++){
+        // deal 7 random cards to each player
+        // NOTE: pass by address to decks
+      }
+    
+    break;
+  
+    case 0x2; // Setup for Rummy
+      
+    break;
+
+    case 0x3: // Setup for... game 3?
+
+    break;
+}
+
 int* get_deck(int player_ID){
   /*
   this will get the deck from player player_ID (number between 1 and 4).
