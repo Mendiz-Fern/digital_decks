1cde70aaf4487cb60e797f29c76d13b24e1dc07f
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 15:33:05 -0400

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+// Main file
+
+#include <stdio.h>
+#include "game_functions.h"
+#include "player_functions.h"
+
+int main(int argc, char* argv[]){
+  char* decks[4]; //Create 4 decks of variable length that include the card IDs. Deck 0 is player 1's deck, 1 is player 2's deck, etc.
+  
+  
+}
