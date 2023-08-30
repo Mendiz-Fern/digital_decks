c656a88adc11707e7b10799e7ca73bd04a2043ee
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 14:55:50 -0400

    Added two functions

--- game_functions.h
+++ game_functions.h
@@ -1,1 +1,7 @@
+// "These are the function declarations for game_functions.c. The documentation for each function will be in game_functions.c" -Fern
+
 #include <stdio.h>
+
+int* select_card(int* deck, int pos_in_deck);
+
+int* play_card();
