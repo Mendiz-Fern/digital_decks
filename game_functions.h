1be7512d4e9b853a79cc337bd6a1c717792d0d24
Mendiz-Fern <97479295+Mendiz-Fern@users.noreply.github.com>
2023-08-30 16:00:18 -0400

    Update game_functions.h

--- game_functions.h
+++ game_functions.h
@@ -2,6 +2,12 @@
 
 #include <stdio.h>
 
+struct _card {
+  int number;
+  char* suit; // Also applies for color
+  char* ability; // Game-dependent
+} Card
+
 char* deal(num_cards);
 void setup_game(char game_ID, char* decks[4], int num_players);
 int* get_deck(int player_ID);
